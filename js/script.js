9ef77e46b383c89e457a765bb7c305c172365d82
Your Name <you@example.com>
2020-02-22 18:31:37 +0300

    Execute hide and show methods

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+    $("#d-content").show();
+    $("#design").hide();
+    });
+    $("#d-content").click(function(){
+    $("#design").show();
+    $("#d-content").hide();
+    })
+
+    $("#development").click(function(){
+    $("#de-content").show();
+    $("#development").hide();
+    });
+    $("#de-content").click(function(){
+    $("#development").show();
+    $("#de-content").hide();
+    }) 
+
+    $("#product").click(function(){
+    $("#p-content").show();
+    $("#product").hide();
+    });
+    $("#p-content").click(function(){
+    $("#product").show();
+    $("#p-content").hide();
+    }) 
+}) 
